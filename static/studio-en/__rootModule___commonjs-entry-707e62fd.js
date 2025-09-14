20f8a3de4216483925a45599fcf40edf6b905d91
Rick McGeer <rick@mcgeer.com>
2025-09-14 15:56:24 -0700

    Build 2025.09.14

---
+++ static/studio-en/__rootModule___commonjs-entry-707e62fd.js
@@ -0,0 +1,14 @@
+System.register(['./__rootModule___commonjs-entry-cf084404.js'], (function (exports) {
+	var __contextModule__ = typeof module !== 'undefined' ? module : arguments[1];
+;
+	return {
+		setters: [function (module) {
+			exports('renderFrozenPart', module.gH);
+		}],
+		execute: (function () {
+
+
+
+		})
+	};
+}));
